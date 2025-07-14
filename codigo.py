2bcf1cce341846e2da25dcd40fd908b9ee147bc9
juan <juanfelipeperdomo41@gmail.com>
2025-07-14 13:11:18 -0500

    añadiendo texto al codigo de python

--- codigo.py
+++ codigo.py
@@ -1,4 +1,5 @@
 nombre = "Juan"
 apellido = "Perdomo"
 
-print("Hola", nombre + ",", apellido, "¿cómo estás?")
+print("Hola", nombre + ",", apellido, "¿cómo estás todo bien o que?")
+print("un justo conocerte")
